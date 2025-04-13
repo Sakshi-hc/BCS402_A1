cb5bf6df2bddbd6366403ff69d79b6e52f8cb125
Sakshi-hc <sakshi.23cs130@sode-edu.in>
2025-04-13 19:30:41 +0530

    Update 9Que.asm

--- 9Que.asm
+++ 9Que.asm
@@ -1,10 +1,10 @@
-        AREA SwapCode, CODE, READONLY ; Define code section
-        ENTRY                        ; Mark entry point
+        AREA SwapCode, CODE, READONLY 
+ENTRY                      
         
-        LDR r2, =0X40000000          ; Load address 0X40000000 into r2
-        LDR r0, =0x00000000          ; Initialize r0 with 0x00000000
-        LDR r1, =0x11112222          ; Initialize r1 with 0x11112222
+        LDR r2, =0X40000000          
+        LDR r0, =0x00000000          
+        LDR r1, =0x11112222        
 
-        SWP r0, r1, [r2]             ; Swap r1 with memory at [r2] and store old memory value in r0
+        SWP r0, r1, [r2]            
 
-        END                          ; End of program
+        END                       
